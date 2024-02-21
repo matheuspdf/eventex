059457b1ec42352b7af4e6a7be29f259032dabd2
matheuspdf <matheuslopes.pdf@gmail.com>
2024-02-20 22:42:31 -0300

    Tela de inscrição

--- eventex/subscriptions/tests.py
+++ eventex/subscriptions/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from eventex.subscriptions.forms import SubscriptionForm
+from django.core import mail
+
 
 class SubscribeTest(TestCase):
     def setUp(self):
@@ -17,7 +19,7 @@ class SubscribeTest(TestCase):
         """Html must contain input tags"""
         self.assertContains(self.resp, '<form')
         self.assertContains(self.resp, '<input', 6)
-        self.assertContains(self.resp, 'type="text"',3)
+        self.assertContains(self.resp, 'type="text"', 3)
         self.assertContains(self.resp, 'type="email"')
         self.assertContains(self.resp, 'type="submit"')
 
@@ -30,7 +32,46 @@ class SubscribeTest(TestCase):
         form = self.resp.context['form']
         self.assertIsInstance(form, SubscriptionForm)
 
-    def test_has_form_fields(self):
+    def test_form_has_fields(self):
         """Form must have 4 fields"""
         form = self.resp.context['form']
         self.assertSequenceEqual(['name', 'cpf', 'email', 'phone'], list(form.fields))
+
+
+class SubscribePostTest(TestCase):
+    def setUp(self):
+        data = dict(name='Matheus', cpf='12345678901', email='matheus@gmail.com', phone='11 984028729')
+        self.resp = self.client.post('/inscricao/', data)
+
+    def test_post(self):
+        """Valid POST should redirect to /incricao/"""
+        self.assertEqual(302, self.resp.status_code)
+
+    def test_send_subscribe_email(self):
+        self.assertEqual(1, len(mail.outbox))
+
+    def test_subscription_email_subject(self):
+        email = mail.outbox[0]
+        expect = 'Confirmação de inscrição'
+
+        self.assertEqual(expect, email.subject)
+
+    def test_subscription_email_from(self):
+        email = mail.outbox[0]
+        expect = 'contato@eventex.com.br'
+
+        self.assertEqual(expect, email.from_email)
+
+    def test_subscription_email_to(self):
+        email = mail.outbox[0]
+        expect = ['contato@eventex.com.br', 'matheus@lopes.com']
+
+        self.assertEqual(expect, email.to)
+
+    def test_subscription_email_body(self):
+        email = mail.outbox[0]
+
+        self.assertIn('Matheus', email.body)
+        self.assertIn('12345678901', email.body)
+        self.assertIn('contato@eventex.com.br', email.body)
+        self.assertIn('11984028729', email.body)
--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
-
+from django.http import HttpResponseRedirect
 from eventex.subscriptions.forms import SubscriptionForm
+from django.core import mail
 
 
 def subscribe(request):
-    context = {'form': SubscriptionForm()}
-    return render(request, 'subscriptions/subscription_form.html', context)
+    if request.method == 'POST':
+        mail.send_mail('Confirmação de inscrição',
+                       MESSAGE,
+                       'contato@eventex.com.br',
+                       ['contato@eventex.com.br', 'matheus@lopes.com'])
+        return HttpResponseRedirect('/inscricao/')
+    else:
+        context = {'form': SubscriptionForm()}
+        return render(request, 'subscriptions/subscription_form.html', context)
+
+
+MESSAGE = """
+Email teste de Matheus
+12345678901
+contato@eventex.com.br
+11984028729
+"""
