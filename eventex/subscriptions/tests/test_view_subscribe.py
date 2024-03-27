f2f80160532d7d9aba7859440b49c82b7c8f646b
matheuspdf <matheuslopes.pdf@gmail.com>
2024-03-27 20:01:04 -0300

    Thank you page

--- eventex/subscriptions/tests/test_view_subscribe.py
+++ eventex/subscriptions/tests/test_view_subscribe.py
@@ -1,7 +1,7 @@
-from django.test import TestCase
-from eventex.subscriptions.forms import SubscriptionForm
 from django.core import mail
+from django.test import TestCase
 
+from eventex.subscriptions.forms import SubscriptionForm
 from eventex.subscriptions.models import Subscription
 
 
@@ -47,8 +47,8 @@ class SubscribePostValid(TestCase):
         self.resp = self.client.post('/inscricao/', data)
 
     def test_post(self):
-        """Valid POST should redirect to /incricao/"""
-        self.assertEqual(302, self.resp.status_code)
+        """Valid POST should redirect to /incricao/1/"""
+        self.assertRedirects(self.resp, '/inscricao/1/')
 
     def test_send_subscribe_email(self):
         self.assertEqual(1, len(mail.outbox))
@@ -78,11 +78,3 @@ class SubscribePostInvalid(TestCase):
 
     def test_dont_save_subscription(self):
         self.assertFalse(Subscription.objects.exists())
-
-
-class SubscribeSuccessMessage(TestCase):
-    def test_message(self):
-        data = dict(name='Matheus', cpf='12345678901',
-                    email='matheus@lopes.com', phone='11-984028729')
-        response = self.client.post('/inscricao/', data, follow=True)
-        self.assertContains(response, 'Inscrição realizada com sucesso!')
