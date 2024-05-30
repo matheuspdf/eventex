9b3d36b0a38056476664fea35e29886cd8b4e9ec
matheuspdf <matheuslopes.pdf@gmail.com>
2024-05-30 18:21:22 -0300

    Keynote speakers #103

--- eventex/core/tests/test_view_home.py
+++ eventex/core/tests/test_view_home.py
@@ -23,3 +23,20 @@ class HomeTest(TestCase):
     def test_subscription_link(self):
         expected = f'href="{r("subscriptions:new")}"'
         self.assertContains(self.response, expected)
+
+    def test_speakers(self):
+        """Must show keynote speakers"""
+        contents = [
+            'Grace Hopper',
+            'https://www.timeforkids.com/wp-content/uploads/2020/08/Grace_003.jpg?w=926',
+            'Alan Turing',
+            'https://cdn.britannica.com/81/191581-050-8C0A8CD3/Alan-Turing.jpg',
+        ]
+
+        for expected in contents:
+            with self.subTest():
+                self.assertContains(self.response, expected)
+
+    def test_speakers_link(self):
+        expected = 'href="{}#speakers"'.format(r('home'))
+        self.assertContains(self.response, expected)
