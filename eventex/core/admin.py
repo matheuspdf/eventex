addc5049069718e67f5688720845268107c6b09b
matheuspdf <matheuslopes.pdf@gmail.com>
2024-06-19 20:01:24 -0300

    Talk

--- eventex/core/admin.py
+++ eventex/core/admin.py
@@ -1,4 +1,4 @@
-from eventex.core.models import Speaker, Contact
+from eventex.core.models import Speaker, Contact, Talk
 from django.contrib import admin
 from django.utils.html import format_html
 
@@ -25,3 +25,4 @@ class SpeakerModelAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Speaker, SpeakerModelAdmin)
+admin.site.register(Talk)
